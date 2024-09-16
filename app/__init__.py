e51d221fc743fd96ae5ed375fea575da89da3b0f
pheineke <heineke.pe@gmail.com>
2024-09-16 19:52:07 +0200

    added route in app

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
+    from app.home_controller import home
+    app.register_blueprint(home, url_prefix='/')
     
     return app
