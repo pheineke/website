d8bdbe84b820ddde21fffa3751b0e718d7024632
pheineke <heineke.pe@gmail.com>
2024-09-16 19:46:39 +0200

    update to sh

--- update_app.sh
+++ update_app.sh
@@ -6,14 +6,14 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
 
 if [ $LOCAL != $REMOTE ]; then
-    echo "## $(date '+%Y-%m-%d %H:%M:%S') - Updates found" >> update.log
+    echo "## $(date '+%Y-%m-%d %H:%M:%S') - Updates found" >> update.md
     # Get the list of new commits
-    git log --oneline --pretty=format:"- %h %s" $LOCAL..$REMOTE >> update.log
+    git log --oneline --pretty=format:"- %h %s" $LOCAL..$REMOTE >> update.md
     echo "" >> update.log  # Add a new line after the commit list
     # Pull the updates
-    git pull origin main >> update.log 2>&1
+    git pull origin main >> update.md 2>&1
     # Restart Gunicorn service
     sudo systemctl restart flaskapp
 else
-    echo "## $(date '+%Y-%m-%d %H:%M:%S') - No updates found" >> update.log
+    echo "## $(date '+%Y-%m-%d %H:%M:%S') - No updates found" >> update.md
 fi
